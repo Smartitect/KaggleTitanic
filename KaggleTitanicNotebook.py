dd920e897e144a554e4d442faebc147dd812bbce
Smartitect <barry.j.a.smart@gmail.com>
2020-01-27 10:41:01 +0000

    Tidying up sections.

--- KaggleTitanicNotebook.py
+++ KaggleTitanicNotebook.py
@@ -1,18 +1,49 @@
+# %%
+# =============================================================================
 # %% [markdown]
-# # Introduction...
+# # Part 1 - Introduction - Look at the Big Picture
 #
 # In this notebook I'm attempting my first Kaggle competition.
 #
+# ## 1.1 - Frame The Problem
+#
+# This problem can be classified as follows:
+# - A **classfication** problem : we are being asked to predict whether an individual dies or not based on a range of continuous and categorical features.  Indeed to be more specific this is a **univariate classifcaiton** problem : ie we are being asked to predcict a single label for each individual;
+# - **Supervised** learning : we are given a training data set with labeled examples;
+# - **Batch** Learning : we will run the models as a one off as the data is static and will never change;
+#
+# Wider non-functional requirements indicate:
+# - Low data volume - 
+# - Non-sensitive data - 
+# - No legal or ethical concerns - 
+#
+# The types of model that are likely to support this problem are therefore:
+# - X
+# - Y
+# - Z
+# 
 # Key things I wanted to achieve:
-# 1. Use of Microsoft's DataPrep SDK for Python rather than more traditional data wrangling methods - a much more scalable framework;
-# 2. Application of a simple linear regression model initially;
-# 3. Moving onto more sophisticated models later on.
-
+# 1. Use of Microsoft's DataPrep SDK for Python rather than more traditional data wrangling methods - taking advantage of some of the advanced features such as "column by example";
+# 2. Start with application of a simple linear regression model and then build more sophisticated models later on.
+#
+# ## 1.2 - Select a Performance Measure
+#
+# The performance measure is driven by Kaggle: that is the overall **accuracy** of the model - defined as the percentage of people who are labeled correctly.
+# It would also be interesting to see more detailed metrics - ie the confusion matirix.
+# Also want to use advanced techniques for hyperparameter tuning - more on this later.
+# 
+# ## 1.3 - List and Validate The Assumptions
+# 
+# - Assumption 1 - 
+# 
 # ## Pre-requisites
 #
-# To install the Microsoft Azure ML Dataprep package:
+# The major non-standard module used is the 'azurelm.dataprep' SDK.  To install the Microsoft Azure ML Dataprep package:
 #
-# pip install azureml-dataprep
+# `pip install azureml-dataprep`
+#
+# Documentation is available at the following web sites:
+#
 
 # %%
 import azureml.dataprep as dprep
@@ -21,6 +52,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+from sklearn.ensemble import RandomForestClassifier
 
 # %% [markdown]
 # Input data files are available in the "../input/" directory.
@@ -32,10 +64,9 @@ for dirname, _, filenames in os.walk('./kaggle/input'):
         print(os.path.join(dirname, filename))
 
 
-# %%
 # =============================================================================
 # %% [markdown]
-# # Part 1 - Data Ingestion
+# # Part 2 - Data Ingestion
 
 # %%
 # Load the train and test files as Pandas dataframes...
@@ -51,11 +82,17 @@ dataflow = dprep.auto_read_file('./kaggle/input/train.csv')
 dataflow.get_profile()
 
 # %% [markdown]
-# %%
 # =============================================================================
-# # Part 2 - Feature Engineering
-
-# %%
+# # Part 3 - Feature Engineering I
+# 
+# Feature engineering necessary to support exploratory data analysis (EDA) below.
+#
+# In this section a range of actions are performed such as:
+# 1. Feature Extraction - pulling "Title" from the full name of each passenger;
+# 2. Feature Engineering - imputing missing ages a passenger based on the mean age for the group of passengers to which that passenger is aligned;
+# 3. Feature Extraction - counting cabin occupancy and passengers who also shared the same ticket;
+# 4. Feature Extract - pulling the "Level" from the cabin (if indeed the passenger had a cabin);
+#
 # =============================================================================
 # %% [markdown]
 # ## Creation of "Title" feature.
@@ -65,17 +102,17 @@ dataflow.get_profile()
 def create_examples(df, builder, column, search_example_pairs):
     example_list = []
     for key, value in search_example_pairs.items():
-        example = df_train[df_train[column].str.contains(key)].iloc[0][column]
+        example = df[df[column].str.contains(key)].iloc[0][column]
         builder.add_example(source_data = {column: example}, example_value = value)
 
 # %%
 df_train[df_train['Name'].str.contains('Jonkheer')].iloc[0]['Name']
+
 #%%
 df_train.iloc[38]
 
 #%%
 df_train.iloc[759]
-
 
 # Use the following to find specific titles in the name column - this helps to select examples to train the "derive by column" approach.
 #df_title_examples[df_title_examples['Name'].str.contains("Countess")]
@@ -86,7 +123,7 @@ title_search_example_pairs = {
     'Mr': 'Mr',
     'Mrs': 'Mrs',
     'Miss': 'Miss',
-    'Vander Planke, Miss': 'Miss',
+    'Vander Planke, Miss': 'Miss', 
     'Rothes, the Countess. of': 'Countess',
     'Master': 'Master' }
 
@@ -98,18 +135,13 @@ dataflow = builder_title.to_dataflow()
 profile = dataflow.get_profile()
 profile.columns['Title'].value_counts
 
-# %%
 # =============================================================================
 # %% [markdown]
 # ## Creation of "Cabin Occupancy" Feature
 
-# %%
-dataflow_filtered = dataflow.filter((dataflow_train_add_title['Cabin'] != ''))
-dataflow_filtered.get_profile()
-
 #%%
 # Add in a CabinOccupancy using a count.
-dataflow_filtered = dataflow_filtered.summarize(
+dataflow = dataflow.summarize(
         summary_columns=[
             dprep.SummaryColumnsValue(
                 column_id='PassengerId',
@@ -117,56 +149,26 @@ dataflow_filtered = dataflow_filtered.summarize(
                 summary_function=dprep.SummaryFunction.COUNT)],
         group_by_columns=['Cabin'],
         join_back=True)
-dataflow_filtered = dataflow_filtered.keep_columns(['PassengerId', 'CabinOccupancy'])
 
-# %%
-# Use a left outer join to add the new CabinOccupancy column back into the main data set.
-dataflow = dprep.Dataflow.join(left_dataflow=dataflow
-                                      right_dataflow=dataflow_filtered,
-                                      join_key_pairs=[('PassengerId', 'PassengerId')],
-                                      left_column_prefix=None,
-                                      right_column_prefix='r_',
-                                      join_type=dprep.JoinType.LEFTOUTER)
-
-# %%
-# Tidy up by filling nulls, dropping unwanted column and renaming new column.
-dataflow = dataflow.fill_nulls('r_CabinOccupancy', 0)
-dataflow = dataflow.drop_columns('r_PassengerId')
-dataflow = dataflow.rename_columns({'r_CabinOccupancy': 'CabinOccupancy'})
-
-
-# %%
-# Use the following to view the result of the cell above based on a specific cabin.
-#df_examples = dataflow_train_new_features.to_pandas_dataframe()
-#df_examples[df_examples['Cabin'].str.contains("G6")]
-
-# %%
 # =============================================================================
 # %% [markdown]
 # ## Creation of "Level" Feature
 
 # %%
-df_level_examples = dataflow.to_pandas_dataframe()
-df_level_examples.head(20)
-
-# %%
-df_level_examples.tail(20)
+dataflow = dataflow.fill_nulls('Cabin', 'None')
 
 # %%
 builder_level = dataflow.builders.derive_column_by_example(source_columns = ['Cabin'], new_column_name = 'Level')
 # builder_level.add_example(source_data = df.iloc[0], example_value = 'None')
-builder_level.add_example(source_data = df_level_examples.iloc[0], example_value = 'C')
-builder_level.add_example(source_data = df_level_examples.iloc[2], example_value = 'E')
-builder_level.add_example(source_data = df_level_examples.iloc[879], example_value = None)
+builder_level.add_example(source_data = {'Cabin': 'C85'}, example_value = 'C')
+builder_level.add_example(source_data = {'Cabin': 'E46'}, example_value = 'E')
+builder_level.add_example(source_data = {'Cabin': 'None'}, example_value = 'None')
+builder_level.add_example(source_data = {'Cabin': None}, example_value = 'None')
 builder_level.preview() # Preview top 10 rows.
 
 # %%
 dataflow = builder_level.to_dataflow()
 
-# %%
-dataflow = dataflow.fill_nulls('Level', 'None')
-
-# %%
 # =============================================================================
 # %% [markdown]
 # ## Create "People On Ticket" Feature
@@ -179,17 +181,15 @@ dataflow = dataflow.summarize(
                 summary_function=dprep.SummaryFunction.COUNT)],
         group_by_columns=['Ticket'],
         join_back=True)
-dataflow.get_profile()
 
-# %%
 # =============================================================================
 # %% [markdown]
 # ## Impute Missing Values "Age" Feature
 
-# impute with MEAN
+# Configure the impute for the Age column using MEAN
 impute_mean = dprep.ImputeColumnArguments(column_id='Age',
                                           impute_function=dprep.ReplaceValueFunction.MEAN)
-# get instance of ImputeMissingValuesBuilder
+# Apply the impute and define grouping
 builder_impute_age = dataflow.builders.impute_missing_values(impute_columns=[impute_mean],
                                                    group_by_columns=['Title'])
 
@@ -199,9 +199,22 @@ builder_impute_age.learn()
 # call to_dataflow() to get a dataflow with impute step added
 dataflow = builder_impute_age.to_dataflow()
 
+# %%
+dataflow
 
+# %%
+dataflow.get_profile()
+
+# %%
+dataflow_test = dataflow.replace_datasource(dprep.LocalDataSource('./kaggle/input/test.csv'))
+
+# %%
+profile = dataflow_test.get_profile()
+profile.columns['Title'].value_counts
+
+# =============================================================================
 # %% [markdown]
-# # Part 3 - Exploratory Data Analysis (EDA)
+# # Part 4 - Exploratory Data Analysis (EDA)
 #
 # Now going to work through each column in the data set to get an insight into it.
 
@@ -264,8 +277,18 @@ generate_box_swarm_plot(df, 'Level', 'Fare', 'Survived')
 # %%
 generate_box_swarm_plot(df, 'PeopleOnTicket', 'Age', 'Survived')
 
-# %%
-from sklearn.ensemble import RandomForestClassifier
+# =============================================================================
+# %% [markdown]
+# # Part 5 - Feature Engineering II
+#
+# Now going to work through each column in the data set to get an insight into it.
+#
+#
+# =============================================================================
+# %% [markdown]
+# # Part 5 - Exploratory Data Analysis (EDA)
+#
+# Now going to work through each column in the data set to get an insight into it.
 
 y = df["Survived"]
 
